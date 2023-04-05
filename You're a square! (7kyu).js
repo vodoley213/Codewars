d0f912e379aac37838c66de90f24b90eaae74934
vodoley <lampiduso@gmail.com>
2023-04-05 12:35:45 +0700

    7kyu Kata solved (You're a square!)

---
+++ You're a square! (7kyu).js
@@ -0,0 +1,24 @@
+// ----- You're a square! (7kyu) -----
+
+// Given an integral number, determine if it's a square number:
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+//https://www.codewars.com/kata/54c27a33fb7da0db0100040e/
+
+var isSquare = function (n) {
+  const isInteger = Number.isInteger(Math.sqrt(n))? true : false
+  return isInteger
+}
+
+console.log(isSquare(26))
+
+
